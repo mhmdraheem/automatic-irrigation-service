cb260bd9143d45720f1411ba0ddd8acb9e499ca6
m.baset@zoho.com <m.baset@zoho.com>
2023-07-09 20:41:40 +0300

    creating project skeleton

---
+++ src/main/java/com/banquemisr/irrigationservice/AutomaticIrrigationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.banquemisr.irrigationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticIrrigationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaticIrrigationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banquemisr/irrigationservice/AutomaticIrrigationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banquemisr.irrigationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticIrrigationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
